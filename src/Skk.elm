7a7177e5b514384c7dd428a9b1da33d6a60bef54
emanon001 <emanon001@gmail.com>
2023-01-14 23:43:31 +0900

    feat: SkkModeのデータ構造を変更

--- src/Skk.elm
+++ src/Skk.elm
@@ -1,4 +1,4 @@
-module Skk exposing (AsciiModeValue, HiraganaModeValue, Skk, SkkContext, SkkHenkanMode(..), SkkInputKey, SkkInputMode(..), init, update)
+module Skk exposing (AsciiInputModeValue, HiraganaInputModeValue, Skk, SkkContext, SkkInputKey, SkkMode(..), init, update)
 
 import Regex
 import SkkDict
@@ -6,7 +6,7 @@ import SkkKanaRule
 
 
 type alias Skk =
-    { inputMode : SkkInputMode
+    { mode : SkkMode
     , context : SkkContext
     }
 
@@ -17,19 +17,44 @@ type alias SkkContext =
     }
 
 
-type SkkInputMode
-    = AsciiMode AsciiModeValue
-    | HiraganaMode HiraganaModeValue
+type SkkMode
+    = AsciiInputMode AsciiInputModeValue
+    | HiraganaInputMode
+        -- ローマ字からひらがなに変換する確定入力モード
+        HiraganaInputModeValue
+    | HiraganaMidashiInputMode
+        -- 辞書変換の対象となるひらがなの見出し語を入力するモード
+        HiraganaMidashiInputModeValue
+    | HiraganaDictHenkanMode
 
 
-type alias AsciiModeValue =
+
+-- ひらがなの見出し語について辞書変換を行うモード
+
+
+type alias AsciiInputModeValue =
     { input : String
     }
 
 
-type alias HiraganaModeValue =
-    { input : String
-    , henkanMode : SkkHenkanMode
+type alias HiraganaInputModeValue =
+    { kakutei : String
+    , mikakutei : String
+    }
+
+
+type alias HiraganaMidashiInputModeValue =
+    { prevMode : HiraganaInputModeValue
+    , kakutei : String -- 確定した見出し語
+    , mikakutei : String -- 未確定の見出し語
+    , okuri : String -- 送り仮名
+    }
+
+
+type alias HiraganaDictHenkanModeValue =
+    { prevMode : HiraganaMidashiInputModeValue
+    , candidateList : SkkDict.SkkDictCandidateList
+    , pos : Int
     }
 
 
@@ -40,28 +65,13 @@ type alias SkkInputKey =
     }
 
 
-type SkkHenkanMode
-    = -- 確定入力モード。ローマ字から『ひらがな』または『カタカナ』に変換するモード
-      KakuteiInputMode String
-      -- 辞書変換の対象となる見出し語を入力するモード
-    | MidashiInputMode
-        { midashi : String
-        , okuri : String
-        }
-      -- 見出し語について辞書変換を行うモード
-    | DictHenkanMode
-        { candidateList : SkkDict.SkkDictCandidateList
-        , pos : Int
-        }
-
-
 
 -- factory
 
 
 init : SkkContext -> Skk
 init context =
-    { inputMode = AsciiMode { input = "" }
+    { mode = AsciiInputMode { input = "" }
     , context = context
     }
 
@@ -72,63 +82,65 @@ init context =
 
 update : Skk -> SkkInputKey -> Skk
 update skk key =
-    case skk.inputMode of
-        AsciiMode value ->
-            { skk | inputMode = updateAsciiMode value key }
+    case skk.mode of
+        AsciiInputMode value ->
+            { skk | mode = updateAsciiInputMode value key }
 
-        HiraganaMode value ->
-            { skk | inputMode = updateHiraganaMode value key }
+        HiraganaInputMode value ->
+            { skk | mode = updateHiraganaInputMode value key }
+
+        _ ->
+            skk
 
 
-updateAsciiMode : AsciiModeValue -> SkkInputKey -> SkkInputMode
-updateAsciiMode value inputKey =
+updateAsciiInputMode : AsciiInputModeValue -> SkkInputKey -> SkkMode
+updateAsciiInputMode value inputKey =
     let
         asciiKey =
             Regex.fromString "^[a-zA-Z0-9 +=!@#$%^&*()\\-_`~\\|'\":;[\\]{}?/.,<>]$" |> Maybe.withDefault Regex.never
     in
     if isSwitchToKanaModeKey inputKey then
-        HiraganaMode { input = value.input, henkanMode = KakuteiInputMode "" }
+        HiraganaInputMode { kakutei = value.input, mikakutei = "" }
 
     else if Regex.contains asciiKey inputKey.key then
-        AsciiMode { input = value.input ++ inputKey.key }
+        AsciiInputMode { input = value.input ++ inputKey.key }
 
     else if isBackSpaceKey inputKey then
-        AsciiMode { input = applyBackSpace value.input }
+        AsciiInputMode { input = applyBackSpace value.input }
 
     else
         -- ignore
-        AsciiMode { input = value.input }
+        AsciiInputMode { input = value.input }
 
 
-updateHiraganaMode : HiraganaModeValue -> SkkInputKey -> SkkInputMode
-updateHiraganaMode value inputKey =
+updateHiraganaInputMode : HiraganaInputModeValue -> SkkInputKey -> SkkMode
+updateHiraganaInputMode value inputKey =
     if isSwitchToHenkanModeKey inputKey then
         -- TODO
         -- (a) 確定入力モード → 見出し語入力モード
         -- (b) 見出し語入力モードで送り仮名の位置を指定
-        HiraganaMode value
+        HiraganaInputMode value
 
     else if isHenkanAcceptedKey inputKey then
         -- TODO
         -- (a) 確定入力モード: ひらがな変換
         -- (b) 見出し語入力モード: 送り仮名なし
         -- (c) 見出し語入力モード: 送り仮名あり
-        case value.henkanMode of
-            KakuteiInputMode roma ->
-                HiraganaMode value
-
-            _ ->
-                HiraganaMode value
+        HiraganaInputMode value
 
     else if isBackSpaceKey inputKey then
-        HiraganaMode { value | input = applyBackSpace value.input }
+        let
+            ( kakutei, mikakutei ) =
+                applyBackSpaceForKana value.kakutei value.mikakutei
+        in
+        HiraganaInputMode { value | kakutei = kakutei, mikakutei = mikakutei }
 
     else if isSpaceKey inputKey then
-        HiraganaMode { value | input = value.input ++ " " }
+        HiraganaInputMode { value | kakutei = value.kakutei ++ " ", mikakutei = "" }
 
     else
         -- ignore
-        HiraganaMode value
+        HiraganaInputMode value
 
 
 
@@ -168,5 +180,14 @@ isBackSpaceKey { key } =
 
 
 applyBackSpace : String -> String
-applyBackSpace key =
-    String.dropRight 1 key
+applyBackSpace input =
+    String.dropRight 1 input
+
+
+applyBackSpaceForKana : String -> String -> ( String, String )
+applyBackSpaceForKana kakutei mikakutei =
+    if String.isEmpty mikakutei then
+        ( String.dropRight 1 kakutei, mikakutei )
+
+    else
+        ( kakutei, String.dropRight 1 mikakutei )
--- tests/SkkTests.elm
+++ tests/SkkTests.elm
@@ -14,7 +14,7 @@ suite =
             [ test "initialize Skk" <|
                 \_ ->
                     Expect.equal
-                        { inputMode = Skk.AsciiMode { input = "" }
+                        { mode = Skk.AsciiInputMode { input = "" }
                         , context =
                             { kanaRules = SkkKanaRule.getDefaultRules
                             , dict = Dict.empty
@@ -23,69 +23,79 @@ suite =
                         (Skk.init { kanaRules = SkkKanaRule.getDefaultRules, dict = Dict.empty })
             ]
         , describe "Skk.update"
-            [ describe "ascii mode"
+            [ describe "Ascii入力モード"
                 [ test "入力したキーが確定済みの文字列に追加されること" <|
                     \_ ->
                         let
                             skk =
-                                initSkk (Skk.AsciiMode { input = "" })
+                                initSkk (Skk.AsciiInputMode { input = "" })
 
                             key =
                                 { key = "a", shift = False, ctrl = False }
                         in
-                        Expect.equal { skk | inputMode = Skk.AsciiMode { input = "a" } } (Skk.update skk key)
+                        Expect.equal { skk | mode = Skk.AsciiInputMode { input = "a" } } (Skk.update skk key)
                 , test "BSキーを入力すると、確定済み文字列の末尾文字が削除されること" <|
                     \_ ->
                         let
                             skk =
-                                initSkk (Skk.AsciiMode { input = "abc" })
+                                initSkk (Skk.AsciiInputMode { input = "abc" })
 
                             key =
                                 { key = "BackSpace", shift = False, ctrl = False }
                         in
-                        Expect.equal { skk | inputMode = Skk.AsciiMode { input = "ab" } } (Skk.update skk key)
+                        Expect.equal { skk | mode = Skk.AsciiInputMode { input = "ab" } } (Skk.update skk key)
                 , test "確定済みの文字列が空の時にBSキーを入力すると、確定済み文字列が空のままになること" <|
                     \_ ->
                         let
                             skk =
-                                initSkk (Skk.AsciiMode { input = "" })
+                                initSkk (Skk.AsciiInputMode { input = "" })
 
                             key =
                                 { key = "BackSpace", shift = False, ctrl = False }
                         in
-                        Expect.equal { skk | inputMode = Skk.AsciiMode { input = "" } } (Skk.update skk key)
+                        Expect.equal { skk | mode = Skk.AsciiInputMode { input = "" } } (Skk.update skk key)
                 , test "Ctrl-jを入力すると、ひらがなモードに切り替わること" <|
                     \_ ->
                         let
                             skk =
-                                initSkk (Skk.AsciiMode { input = "abc" })
+                                initSkk (Skk.AsciiInputMode { input = "abc" })
 
                             key =
                                 { key = "j", shift = False, ctrl = True }
                         in
-                        Expect.equal { skk | inputMode = Skk.HiraganaMode { input = "abc", henkanMode = Skk.KakuteiInputMode "" } } (Skk.update skk key)
+                        Expect.equal { skk | mode = Skk.HiraganaInputMode { kakutei = "abc", mikakutei = "" } } (Skk.update skk key)
                 ]
-            , describe "hiragana mode"
-                [ test "BSキーを入力すると、確定済み文字列の末尾文字が削除されること" <|
+            , describe "ひらがな入力モード"
+                [ test "未確定の文字列が存在しない場合、BSキーを入力すると、確定済み文字列の末尾文字が削除されること" <|
                     \_ ->
                         let
                             skk =
-                                initSkk (Skk.HiraganaMode { input = "あいう", henkanMode = Skk.KakuteiInputMode "" })
+                                initSkk (Skk.HiraganaInputMode { kakutei = "あいう", mikakutei = "" })
 
                             key =
                                 { key = "BackSpace", shift = False, ctrl = False }
                         in
-                        Expect.equal { skk | inputMode = Skk.HiraganaMode { input = "あい", henkanMode = Skk.KakuteiInputMode "" } } (Skk.update skk key)
+                        Expect.equal { skk | mode = Skk.HiraganaInputMode { kakutei = "あい", mikakutei = "" } } (Skk.update skk key)
+                , test "未確定の文字列が存在する場合、BSキーを入力すると、未確定文字列の末尾文字が削除されること" <|
+                    \_ ->
+                        let
+                            skk =
+                                initSkk (Skk.HiraganaInputMode { kakutei = "あいう", mikakutei = "sy" })
+
+                            key =
+                                { key = "BackSpace", shift = False, ctrl = False }
+                        in
+                        Expect.equal { skk | mode = Skk.HiraganaInputMode { kakutei = "あいう", mikakutei = "s" } } (Skk.update skk key)
                 , test "Spaceキーを入力すると、確定済み文字列の末尾にスペースが追加されること" <|
                     \_ ->
                         let
                             skk =
-                                initSkk (Skk.HiraganaMode { input = "あいう", henkanMode = Skk.KakuteiInputMode "" })
+                                initSkk (Skk.HiraganaInputMode { kakutei = "あいう", mikakutei = "s" })
 
                             key =
                                 { key = " ", shift = False, ctrl = False }
                         in
-                        Expect.equal { skk | inputMode = Skk.HiraganaMode { input = "あいう ", henkanMode = Skk.KakuteiInputMode "" } } (Skk.update skk key)
+                        Expect.equal { skk | mode = Skk.HiraganaInputMode { kakutei = "あいう ", mikakutei = "" } } (Skk.update skk key)
                 ]
             ]
         ]
@@ -95,9 +105,9 @@ suite =
 -- helper
 
 
-initSkk : Skk.SkkInputMode -> Skk.Skk
+initSkk : Skk.SkkMode -> Skk.Skk
 initSkk mode =
-    { inputMode = mode
+    { mode = mode
     , context =
         { kanaRules = SkkKanaRule.getDefaultRules
         , dict = Dict.empty
